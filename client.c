e855ca21ff4a139745d62affe754998ddcf3a733
malay44 <malay.p1@ahduni.edu.in>
2024-02-11 22:23:29 +0530

    Refactor client and server code

--- client.c
+++ client.c
@@ -8,25 +8,42 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER_PORT 5433
+#define SERVER_PORT 3000
 #define MAX_LINE 256
+
+void usage(void);
 
 int main(int argc, char *argv[])
 {
-  FILE *fp;
   struct hostent *hp;
   struct sockaddr_in sin;
-  char *host;
+  char *host = NULL;
   char buf[MAX_LINE];
   int s;
   int len;
-  if (argc == 2)
+  int server_port = SERVER_PORT;
+  char c;
+  int file_size = 0;
+
+  while ((c = getopt(argc, argv, "p:h:")) != -1)
   {
-    host = argv[1];
+    switch (c)
+    {
+    case 'p':
+      server_port = atoi(optarg);
+      break;
+    case 'h':
+      host = optarg;
+      break;
+    default:
+      usage();
+      exit(1);
+    }
   }
-  else
+
+  if (host == NULL)
   {
-    fprintf(stderr, "usage: %s host\n", argv[0]);
+    usage();
     exit(1);
   }
 
@@ -38,13 +55,13 @@ int main(int argc, char *argv[])
     exit(1);
   }
   else
-    printf("Client's remote host: %s\n", argv[1]);
+    printf("Client's remote host: %s\n", host);
 
   /* build address data structure */
   bzero((char *)&sin, sizeof(sin));
   sin.sin_family = AF_INET;
   bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
-  sin.sin_port = htons(SERVER_PORT);
+  sin.sin_port = htons(server_port);
 
   /* active open */
   if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
@@ -64,7 +81,7 @@ int main(int argc, char *argv[])
   else
     printf("Client connected.\n");
 
-  /* main loop: get and send lines of text and recive from message from server */
+  /* main loop: get and send lines of text and receive from message from server */
   while (fgets(buf, sizeof(buf), stdin))
   {
     buf[MAX_LINE - 1] = '\0';
@@ -72,20 +89,41 @@ int main(int argc, char *argv[])
     send(s, buf, len, 0);
     if (strncmp(buf, "GET", 3) == 0)
     {
-      if (recv(s, buf, sizeof(buf), 0) == 0)
-      {
-        perror("Server closed connection.\n");
-        close(s);
-        exit(1);
-      }
-      fp = fopen("response.txt", "w");
+      FILE *fp = fopen("response.txt", "w");
       if (fp == NULL)
       {
         perror("Error opening file");
         exit(1);
       }
-      fputs(buf, fp);
+      recv(s, buf, sizeof(buf), 0);
+      printf("File size: %s\n", buf);
+      file_size = atoi(buf);
+      printf("File size: %d\n", file_size);
+      int size_received = 0;
+      while(file_size > 0)
+      {
+        recv(s, buf, sizeof(buf), 0);
+        int len = strlen(buf);
+        file_size -= len;
+        size_received += len;
+        fputs(buf, fp);
+        fflush(fp);
+        // Print progress
+        printf("Received %ld bytes. Remaining: %ld bytes     \r", (long)size_received, (long)file_size);
+        fflush(stdout); // Flush stdout to ensure the message is printed immediately
+      }
       fclose(fp);
+      printf("File size: %d\n", file_size);
+      printf("File received of length %d.\n", size_received);
     }
   }
+  return 0;
+}
+
+void usage(void)
+{
+  printf("Usage:\n");
+  printf(" -p <Port>\n");
+  printf(" -h <Host Address>\n"); // Corrected option description
+  exit(8);
 }
--- server.c
+++ server.c
@@ -13,29 +13,60 @@
 #define MAX_PENDING 5
 #define MAX_LINE 256
 
-int main() {
+void usage(void);
+
+int main(int argc, char *argv[])
+{
   struct sockaddr_in sin;
   char buf[MAX_LINE];
   socklen_t len;
   int s, new_s;
   char str[INET_ADDRSTRLEN];
+  int port = SERVER_PORT;
+  char filename[MAX_LINE] = "sample.txt";
+  char c;
+  struct timeval delay;
+  delay.tv_sec = 0; // Delay in seconds
+  delay.tv_usec = 300;
+
+  while ((c = getopt(argc, argv, "p:f:")) != -1)
+  {
+    switch (c)
+    {
+    case 'p':
+      port = atoi(optarg);
+      break;
+    case 'f':
+      strcpy(filename, optarg);
+      break;
+    default:
+      usage();
+      exit(1);
+    }
+  }
 
   /* build address data structure */
   bzero((char *)&sin, sizeof(sin));
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = INADDR_ANY;
-  sin.sin_port = htons(SERVER_PORT);
-  
+  sin.sin_port = htons(port);
+
   /* setup passive open */
-  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+  {
     perror("simplex-talk: socket");
     exit(1);
   }
- 
-  inet_ntop(AF_INET, &(sin.sin_addr), str, INET_ADDRSTRLEN);
-  printf("Server is using address %s and port %d.\n", str, SERVER_PORT);
 
-  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+  inet_ntop(AF_INET, &(sin.sin_addr), str, INET_ADDRSTRLEN);
+  printf(
+      "Server is using address %s and port %d.\nServer will share file:- %s\n",
+      str,
+      port,
+      filename);
+
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0)
+  {
     perror("simplex-talk: bind");
     exit(1);
   }
@@ -43,31 +74,60 @@ int main() {
     printf("Server bind done.\n");
 
   listen(s, MAX_PENDING);
-  
+
   /* wait for connection, then receive and print text */
-  while(1) {
-    if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0) {
+  while (1)
+  {
+    if ((new_s = accept(s, (struct sockaddr *)&sin, &len)) < 0)
+    {
       perror("simplex-talk: accept");
       exit(1);
     }
     printf("Server Listening.\n");
-    while ((len = recv(new_s, buf, sizeof(buf), 0))){
-      if (strncmp(buf, "GET", 3) == 0) {
+    while ((len = recv(new_s, buf, sizeof(buf), 0)))
+    {
+      if (strncmp(buf, "GET", 3) == 0)
+      {
         printf("GET request received.\n");
-        FILE *fp = fopen("sample.txt", "r");
-        if (fp == NULL) {
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL)
+        {
           perror("Error opening file");
           send(new_s, "Not Found", 9, 0);
         }
-        char content[MAX_LINE];
-        while (fgets(content, sizeof(content), fp) != NULL) {
-          send(new_s, content, strlen(content), 0);
+        fseek(fp, 0L, SEEK_END);
+        int file_size = ftell(fp);
+        fseek(fp, 0L, SEEK_SET);
+        char file_size_str[10];
+        sprintf(file_size_str, "%d", file_size);
+        printf("File size: %s\n", file_size_str);
+        send(new_s, file_size_str, strlen(file_size_str) + 1, 0);
+        select(0, NULL, NULL, NULL, &delay); // Non-blocking delay
+        bzero(buf, sizeof(buf));
+        while (fgets(buf, sizeof(buf), fp))
+        {
+          send(new_s, buf, strlen(buf) + 1, 0);
+          struct timeval delay;
+          delay.tv_sec = 0; // Delay in seconds
+          delay.tv_usec = 300;
+          select(0, NULL, NULL, NULL, &delay); // Non-blocking delay
+          bzero(buf, sizeof(buf));
         }
+        fseek(fp, 0L, SEEK_SET);
       }
       fputs(buf, stdout);
+      bzero(buf, sizeof(buf));
     }
     close(new_s);
   }
   close(s);
   return 0;
+}
+
+void usage(void)
+{
+  printf("Usage:\n");
+  printf(" -p <Port>\n");
+  printf(" -f <File Name>\n");
+  exit(8);
 }
