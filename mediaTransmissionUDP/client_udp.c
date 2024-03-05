77dcf2a8a3e66f9c930f4f040da63b812a16885f
malay44 <malay.p1@ahduni.edu.in>
2024-03-05 23:53:11 +0530

    made mp4 files streamable

--- mediaTransmissionUDP/client_udp.c
+++ mediaTransmissionUDP/client_udp.c
@@ -1,11 +1,12 @@
 /* CSD 304 Computer Networks, Fall 2016
    Lab 2, client
-   Team: 
+   Team:
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -14,75 +15,101 @@
 #define SERVER_PORT 5432
 #define BUF_SIZE 4096
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char * argv[]){
-  
-  FILE *fp;
-  struct hostent *hp;
-  struct sockaddr_in sin;
-  char *host;
-  char buf[BUF_SIZE];
-  int s;
-  int len;
+    FILE *fp;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    char *host;
+    char buf[BUF_SIZE];
+    int s;
+    int len;
 
-  if ((argc==2)||(argc == 3)) {
-    host = argv[1];
-  }
-  else {
-    fprintf(stderr, "usage: client serverIP [download_filename(optional)]\n");
-    exit(1);
-  }
-
-  if(argc == 3) {
-    fp = fopen(argv[2], "w");
-    if (fp == NULL) {
-      fprintf(stderr, "Error opening output file\n");
-      exit(1);
+    if ((argc == 2) || (argc == 3))
+    {
+        host = argv[1];
     }
-  }
+    else
+    {
+        fprintf(stderr, "usage: client serverIP [download_filename(optional)]\n");
+        exit(1);
+    }
 
-  /* translate host name into peer's IP address */
-  hp = gethostbyname(host);
-  if (!hp) {
-    fprintf(stderr, "client: unknown host: %s\n", host);
-    exit(1);
-  }
-  else
-    printf("Host %s found!\n", argv[1]);
+    if (argc == 3)
+    {
+        fp = fopen(argv[2], "wb");
+        if (fp == NULL)
+        {
+            fprintf(stderr, "Error opening output file\n");
+            exit(1);
+        }
+    }
 
-  /* build address data structure */
-  memset((char *)&sin, 0, sizeof(sin));
-  sin.sin_family = AF_INET;
-  memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
-  sin.sin_port = htons(SERVER_PORT);
-  
+    /* translate host name into peer's IP address */
+    hp = gethostbyname(host);
+    if (!hp)
+    {
+        fprintf(stderr, "client: unknown host: %s\n", host);
+        exit(1);
+    }
+    else
+        printf("Host %s found!\n", argv[1]);
 
-  /* create socket */
-  if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("client: socket");
-    exit(1);
-  }
-  
-  printf("Client will get data from to %s:%d.\n", argv[1], SERVER_PORT);
-  printf("To play the music, pipe the download file to a player, e.g., ALSA, SOX, VLC: cat recvd_file.wav | vlc -\n"); 
-  
-  
-  
+    /* build address data structure */
+    memset((char *)&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    memcpy((char *)&sin.sin_addr, hp->h_addr, hp->h_length);
+    sin.sin_port = htons(SERVER_PORT);
 
-  /* send message to server */  
-  fgets(buf, sizeof(buf), stdin);
-  buf[BUF_SIZE-1] = '\0';
-  len = strlen(buf) + 1;
-  if (sendto(s, buf, len, 0, (struct sockaddr *)&sin, sizeof(sin)) < 0) { 
-    perror("Client: sendto()"); 
-    return 0; 
-  }
-    
-  /* get reply, display it or store in a file*/
-  /* Add code to receive unlimited data and either display the data
-     or if specified by the user, store it in the specified file. 
-     Instead of recv(), use recvfrom() call for receiving data */
-  recv(s, buf, sizeof(buf), 0);
-  fputs(buf, stdout);
-  
+    /* create socket */
+    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("client: socket");
+        exit(1);
+    }
+
+    printf("Client will get data from to %s:%d.\n", argv[1], SERVER_PORT);
+    printf("To play the music, pipe the download file to a player, e.g., ALSA, SOX, VLC: cat recvd_file.wav | vlc -\n");
+
+    /* send message to server */
+    fgets(buf, sizeof(buf), stdin);
+    buf[BUF_SIZE - 1] = '\0';
+    len = strlen(buf) + 1;
+    if (sendto(s, buf, len, 0, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    {
+        perror("Client: sendto()");
+        return 0;
+    }
+
+    /* get reply, display it or store in a file*/
+    /* Add code to receive unlimited data and either display the data
+       or if specified by the user, store it in the specified file.
+       Instead of recv(), use recvfrom() call for receiving data */
+    // print message from server
+    recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&sin, &len);
+    printf("Server says: %s\n", buf);
+    ssize_t size_received = 0;
+    socklen_t sin_len = sizeof(sin);
+    while (1)
+    {
+        ssize_t recvLen = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&sin, &sin_len);
+        if (recvLen < 0)
+        {
+            perror("recvfrom");
+            exit(1);
+        }
+        size_received += recvLen;
+        printf("Received %ld bytes\r", size_received);
+        fwrite(buf, 1, recvLen, fp);
+        fflush(fp);
+        if (recvLen < BUF_SIZE)
+        {
+            break;
+        }
+    }
+    printf("File received of size: %ld.\n", size_received);
+    fclose(fp);
+    close(s);
+    return 0;
 }
--- mediaTransmissionUDP/server_udp.c
+++ mediaTransmissionUDP/server_udp.c
@@ -1,23 +1,24 @@
 /* CSD 304 Computer Networks, Fall 2016
    Lab 2, server
-   Team: 
+   Team:
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
-
 #define SERVER_PORT 5432
 #define BUF_SIZE 4096
 
-
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
   struct sockaddr_in sin;
   struct sockaddr_storage client_addr;
   char clientIP[INET_ADDRSTRLEN]; /* For IPv4 addresses */
@@ -28,35 +29,34 @@ int main(int argc, char * argv[]){
   char *host;
   struct hostent *hp;
 
-
-  /* Declarations for file(s) to be sent 
+  /* Declarations for file(s) to be sent
      ...
   */
-  
+
   /* For inserting delays, use nanosleep()
-     struct timespec ... */ 
- 
+     struct timespec ... */
 
   /* To get filename from commandline */
   /* if (argc==...) {} */
-   
-   
+
   /* Create a socket */
-  if ((s = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+  if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+  {
     perror("server: socket");
     exit(1);
   }
- 
- 
+
   /* build address data structure and bind to all local addresses*/
   memset((char *)&sin, 0, sizeof(sin));
   sin.sin_family = AF_INET;
- 
+
   /* If socket IP address specified, bind to it. */
-  if(argc == 2) {
+  if (argc == 2)
+  {
     host = argv[1];
     hp = gethostbyname(host);
-    if (!hp) {
+    if (!hp)
+    {
       fprintf(stderr, "server: unknown host %s\n", host);
       exit(1);
     }
@@ -65,49 +65,66 @@ int main(int argc, char * argv[]){
   /* Else bind to 0.0.0.0 */
   else
     sin.sin_addr.s_addr = INADDR_ANY;
-  
+
   sin.sin_port = htons(SERVER_PORT);
-  
-  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0)
+  {
     perror("server: bind");
     exit(1);
   }
-  else{
+  else
+  {
     /* Add code to parse IPv6 addresses */
     inet_ntop(AF_INET, &(sin.sin_addr), clientIP, INET_ADDRSTRLEN);
     printf("Server is listening at address %s:%d\n", clientIP, SERVER_PORT);
   }
-  
+
   printf("Client needs to send \"GET\" to receive the file %s\n", argv[1]);
-    
-  
+
   client_addr_len = sizeof(client_addr);
-  
+
   /* Receive messages from clients*/
-  while(len = recvfrom(s, buf, sizeof(buf), 0,
-		       (struct sockaddr *)&client_addr, &client_addr_len)){
-    
+  while ((len = recvfrom(s, buf, sizeof(buf), 0,
+                        (struct sockaddr *)&client_addr, &client_addr_len)))
+  {
+
     inet_ntop(client_addr.ss_family,
-	      &(((struct sockaddr_in *)&client_addr)->sin_addr),
-	      clientIP, INET_ADDRSTRLEN);
-    
+              &(((struct sockaddr_in *)&client_addr)->sin_addr),
+              clientIP, INET_ADDRSTRLEN);
+
     printf("Server got message from %s: %s [%d bytes]\n", clientIP, buf, len);
+
+    // send hello message to client
+    sendto(s, "Hello from server", 18, 0, (struct sockaddr *)&client_addr, client_addr_len);
 
     /* Send to client */
     /* Add code to send file if the incoming message is GET */
-    if ((len = sendto(s, buf, sizeof(buf), 0,
-		    (struct sockaddr *)&client_addr, 
-		    client_addr_len)) == -1) {
-      perror("server: sendto");
-      exit(1);
+    printf("Server Listening.\n");
+
+    if (strcmp(buf, "GET\n") == 0)
+    {
+      printf("Server: Sending file to client\n");
+      size_t bytes_read;
+      FILE *fp = fopen("streamable_output.mp4", "rb");
+      if (fp == NULL)
+      {
+        perror("Error opening file");
+        exit(1);
+      }
+      ssize_t size_sent = 0;
+      while ((bytes_read = fread(buf, 1, sizeof(buf), fp)) > 0)
+      {
+        usleep(1600);
+        sendto(s, buf, bytes_read, 0, (struct sockaddr *)&client_addr, client_addr_len);
+        size_sent += bytes_read;
+        printf("Server: Sent %ld bytes\r", size_sent);
+      }
+      printf("Server: File sent of size: %ld\n", size_sent);
+      fclose(fp);
+    } else {
+      printf("Server: Invalid request\n");
     }
-    
     memset(buf, 0, sizeof(buf));
   }
-  
-  /* Send BYE to signal termination */
-  strcpy(buf, "BYE");
-  sendto(s, buf, sizeof(buf), 0, 
-	 (struct sockaddr*)&client_addr, client_addr_len);
-  
 }
