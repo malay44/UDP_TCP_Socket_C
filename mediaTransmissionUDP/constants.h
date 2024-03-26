4584910b0ea8eabea55a4f2f67316c69bad76934
malay44 <malay.p1@ahduni.edu.in>
2024-03-26 13:53:14 +0530

    Update struct definitions in constants.h

--- mediaTransmissionUDP/constants.h
+++ mediaTransmissionUDP/constants.h
@@ -19,37 +19,37 @@ typedef struct
 {
     uint8_t type;
     uint8_t filename_size;
-    char *filename;
+    char filename[255];
 } FileRequest;
 
 typedef struct
 {
     uint8_t type;
     uint8_t num_sequences;
-    uint16_t *sequence_no;
+    uint16_t sequence_no[255];
 } Ack;
 
 typedef struct {
     uint8_t type;
     uint16_t sequence_number;
     uint8_t filename_size;
-    char *filename;
+    char filename[255];
     uint32_t file_size;
     uint16_t block_size;
-    char *data;
+    char data[BUF_SIZE - 1 - 2 - 1 - 255 - 4 - 2];
 } FileInfoAndDataPacket;
 
 typedef struct {
     uint8_t type;
     uint16_t sequence_number;
     uint16_t block_size;
-    char *data;
+    char data[BUF_SIZE - 1 - 2 - 2];
 } DataPacket;
 
 typedef struct {
     uint8_t type;
     uint8_t filename_size;
-    char *filename;
+    char filename[255];
 } FileNotFoundPacket;
 
 #endif /* CONSTANTS_H */
